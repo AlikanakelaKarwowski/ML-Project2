e7b43e25fc38fafb040157de15832e236ac2624e
A.Karwowski <akk42@zips.uakron.edu>
2021-02-28 15:55:03 -0500

    Delete Last Duplicate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from sklearn.linear_model import Lasso, Ridge, LinearRegression
 from sklearn.model_selection import LeaveOneOut, cross_val_predict, train_test_split
-from sklearn.linear_model import LinearRegression
 from sklearn.datasets import load_boston
 from matplotlib import pyplot as plt
 
